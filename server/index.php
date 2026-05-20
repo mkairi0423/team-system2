920bbb64eeb69f242981fd7961d36b2407fb0ba1
kairi <2250159@ecc.ac.jp>
2026-05-20 10:56:27 +0900

    更新４

--- server/index.php
+++ server/index.php
@@ -5,14 +5,19 @@ require_once  __DIR__ . "/../helpers/utils.php";
 ?>
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     test1
     test2
     test3
+    test4
+
 </body>
+
 </html>
